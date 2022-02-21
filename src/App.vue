fed5d2da6f107286555fa35fb72b8efcf716ccfa
winnie <asgh03226@gmail.com>
2022-02-21 17:57:01 +0800

    上傳圖片切版

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+ 
+</script>
+
+<template>
+  <div class="w-1280px h-screen mx-auto p-30  text-center overflow-hidden text-gray-800">
+    <h1 class="mb-10 text-2xl">File Upload & Image Preview</h1>
+    <div class="relative w-1/2 inline-block h-auto  p-4 rounded-md">
+      <label class=" block  rounded-md bg-true-gray-50 border-gray-200 border-2 border-dashed text-gray-400 " for="upload">
+        <input class="hidden" type="file" name="" id="upload">
+        <span class="inline-block leading-200px ">請將圖片拖曳於此</span>
+      </label>
+      <i-mdi-cloud-upload-outline class="absolute  bottom-4 left-0 right-0 mx-auto text-4xl align-middle "/>
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import WindiCSS from 'vite-plugin-windicss'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver';
+import Components from 'unplugin-vue-components/vite'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    Icons({
+    }),
+    WindiCSS(),
+    Components({
+      resolvers: IconsResolver(),
+    }),
+  ]
+})
